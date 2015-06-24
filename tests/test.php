cba929da9da6839299c6aaa755467b9a600bc28b
Philip Kahn <tigerhawkvok@gmail.com>
2015-06-24 16:28:58 -0700

    Some work on tests

--- tests/test.php
+++ tests/test.php
@@ -1,15 +1,33 @@
+<?php
+error_reporting(-1);
+ini_set("display_errors", 1);
+ini_set('error_reporting', -1);
+error_log("handle_misc_edits is running in debug mode!");
+?>
 <!doctype html>
 <html>
   <head>
     <title>
       PHP Core tests
     </title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"/>
   </head>
   <body>
+      <div class='page-header'><h1>PHP Core tests</h1></div>
     <?php
-       error_reporting(E_ALL);
        require_once("../core.php");
-       echo "All is well.";
+       echo "All is well in load-land.<br/>";
+?>
+<p>Beginning tests</p>
+<?php
+# Markdown
+echo "<section class='panel panel-primary'><div class='panel-heading'>Markdown Tests</div>"
+$text = "Here is some **Markdown** text that I *really* want to parse. What about [b]Classic options[/b]?";
+echo "<code>$text</code><br/>";
+$html = Wysiwyg::toHtml($text);
+echo "<div class='bg-success'>$html</div>"
+echo "<br/>Returns: <div class='bg-success'><code>" . Wysiwyg::fromHtml($html) . "</code></div>";
+echo "</section>"
        ?>
   </body>
 </html>
