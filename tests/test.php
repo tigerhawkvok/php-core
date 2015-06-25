7745e90aa422e5dbd55a354d7a3432ed195867b6
Philip Kahn <tigerhawkvok@gmail.com>
2015-06-24 17:14:01 -0700

    Additional escaping options

--- tests/test.php
+++ tests/test.php
@@ -11,6 +11,10 @@ error_log("handle_misc_edits is running in debug mode!");
       PHP Core tests
     </title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"/>
+    <style type="text/css">
+    .panel, .alert{ max-width: 80%; }
+    code { white-space: pre-wrap; }
+    </style>
   </head>
   <body>
       <div class='page-header'><h1>PHP Core tests</h1></div>
@@ -21,13 +25,15 @@ error_log("handle_misc_edits is running in debug mode!");
 <p>Beginning tests</p>
 <?php
 # Markdown
-echo "<section class='panel panel-primary'><div class='panel-heading'>Markdown Tests</div>"
-$text = "Here is some **Markdown** text that I *really* want to parse. What about [b]Classic options[/b]?";
-echo "<code>$text</code><br/>";
+echo "<section class='panel panel-primary center-block'><div class='panel-heading'>Markdown Tests</div>";
+echo "Raw input:<br/><br/>";
+$text = "Here is some **Markdown** text that I *really* want to parse. What about [b]Classic options[/b]? How about greek? [grk]npLl[/grk]";
+echo "<code class='center-block text-center'>$text</code><br/>";
 $html = Wysiwyg::toHtml($text);
-echo "<div class='bg-success'>$html</div>"
-echo "<br/>Returns: <div class='bg-success'><code>" . Wysiwyg::fromHtml($html) . "</code></div>";
-echo "</section>"
+echo "<div class='alert alert-success center-block'>$html</div>";
+echo "<br/>This de-parses as: <div class='alert alert-success center-block'><code>" . Wysiwyg::fromHtml($html) . "</code></div>";
+echo "Full detail: <code>".displayDebug(Wysiwyg::toHtml($text,true),false)."</code>";
+echo "</section>";
        ?>
   </body>
 </html>
